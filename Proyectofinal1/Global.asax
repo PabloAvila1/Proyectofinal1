0f6a99623a3b202dfcca35500772599be72fe351
alumno <alumno@LAB7-4>
2017-05-19 09:26:46 -0600

    Agregar archivos de proyecto.

---
+++ Proyectofinal1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyectofinal1.Global" Language="C#" %>
---
+++ Proyectofinal1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyectofinal1.Startup))]
+namespace Proyectofinal1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
